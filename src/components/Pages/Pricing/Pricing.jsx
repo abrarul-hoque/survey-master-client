3e7fd4f6946adccc4bb64dc04bc6c791a4ef1cf8
Abrarulctg <jubairctg99@gmail.com>
2024-06-03 17:42:48 +0600

    Added pricing and surveys page

---
+++ src/components/Pages/Pricing/Pricing.jsx
@@ -0,0 +1,12 @@
+import SectionTitle from "../../shared/SectionTitle";
+
+const Pricing = () => {
+    return (
+        <div>
+            <SectionTitle subHeading={"Choose the Plan That's Right for You"} heading={"Upgrade to Pro and Unlock Advanced Features"}></SectionTitle>
+
+        </div>
+    );
+};
+
+export default Pricing;
---
+++ src/components/Pages/Surveys/Surveys.jsx
@@ -0,0 +1,10 @@
+
+const Surveys = () => {
+    return (
+        <div>
+            <h1 className='text-3xl'>Surveys are coming soon.....</h1>
+        </div>
+    );
+};
+
+export default Surveys;
