541ff3bdf9b3ba02fdc2a02bfb36bc07af21e61a
Abrarulctg <jubairctg99@gmail.com>
2024-06-03 20:09:04 +0600

    Added pricing plan card on pricing page

--- src/components/Pages/Pricing/Pricing.jsx
+++ src/components/Pages/Pricing/Pricing.jsx
@@ -1,10 +1,46 @@
 import SectionTitle from "../../shared/SectionTitle";
+import { Link } from 'react-router-dom';
 
+import './Pricing.css';
 const Pricing = () => {
     return (
-        <div>
+        <div className="max-w-6xl mx-auto">
             <SectionTitle subHeading={"Choose the Plan That's Right for You"} heading={"Upgrade to Pro and Unlock Advanced Features"}></SectionTitle>
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 my-8">
+                <div className="border p-8 rounded-xl PricingCard">
+                    <h1 className="text-2xl font-bold">Basic Plan - Free</h1>
+                    <div className="divider my-0"></div>
+                    <div>
+                        <ul className="pricingPlanItem">
+                            <li>● Perticipate in Surveys</li>
+                            <li>● Report to a Survey</li>
+                            <li>● Free Register</li>
+                            <li>● Survey Result after Vote</li>
+                        </ul>
+                    </div>
 
+                </div>
+                <div className="border p-8 rounded-xl PricingCard">
+                    <h1 className="text-2xl font-bold">Pro Plan - $25/month </h1>
+                    <div className="divider my-0"></div>
+                    <div>
+                        <ul className="pricingPlanItem">
+                            <li>● Perticipate in Surveys</li>
+                            <li>● Add comment to a Survey</li>
+                            <li>● Report to a Survey</li>
+                            <li>● Like or Dislike to a Survey</li>
+                            <li>● Free Register</li>
+                            <li>● Survey Result after Vote</li>
+                        </ul>
+                        <div className="flex justify-center my-8">
+                            <Link to="/checkout"><button className="btn btn-primary">Upgrade to Pro-User</button></Link>
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
         </div>
     );
 };
--- src/components/shared/SectionTitle.jsx
+++ src/components/shared/SectionTitle.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const SectionTitle = ({ subHeading, heading }) => {
     return (
         <div className='text-center my-8'>
-            <p className="text-warning text-base">{subHeading}</p>
-            <h1 className='text-4xl font-extrabold mb-3'>{heading}</h1>
+            <p className="text-warning text-base mb-4">{subHeading}</p>
+            <h1 className='text-4xl font-extrabold mb-4'>{heading}</h1>
         </div>
     );
 };
