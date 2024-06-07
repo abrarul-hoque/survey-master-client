636d3e32793c791916372f940fe8b16d15ef83f7
Abrarulctg <jubairctg99@gmail.com>
2024-06-07 17:50:06 +0600

    Added payment aprove and change user role functionality

--- src/components/Pages/Dashboard/AdminHome/PaymentHistory.jsx
+++ src/components/Pages/Dashboard/AdminHome/PaymentHistory.jsx
@@ -2,18 +2,47 @@ import React from 'react';
 import useAuth from '../../../../hooks/useAuth';
 import { useQuery } from '@tanstack/react-query';
 import useAxiosSecure from '../../../../hooks/useAxiosSecure';
+import Swal from 'sweetalert2';
 
 const PaymentHistory = () => {
     const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
 
-    const { data: payments = [] } = useQuery({
-        queryKey: ['payments', user.email],
+    const { data: payments = [], refetch } = useQuery({
+        queryKey: ['payments'],
         queryFn: async () => {
-            const res = await axiosSecure.get(`/payments/${user.email}`);
+            const res = await axiosSecure.get(`/payments`);
             return res.data;
         }
     })
+
+
+    const handleAprovePayment = (userEmail, paymentId) => {
+        axiosSecure.patch(`/payments/${paymentId}`)
+            .then(res => {
+                console.log(res.data);
+                if (res.data.modifiedCount > 0) {
+                    axiosSecure.patch(`/users/${userEmail}`)
+                        .then(res => {
+                            console.log(res.data)
+                            refetch();
+                            Swal.fire({
+                                position: "top-end",
+                                icon: "success",
+                                title: `Payment has been Approved`,
+                                showConfirmButton: false,
+                                timer: 1500
+                            });
+                        })
+                }
+
+
+            })
+            .catch(error => {
+                console.log(error)
+            })
+
+    }
 
     return (
         <div>
@@ -25,18 +54,28 @@ const PaymentHistory = () => {
                     <thead className="bg-orange-300">
                         <tr>
                             <th>#</th>
+                            <th>User Email</th>
                             <th>Price</th>
                             <th>Transaction Id</th>
+                            <th>Date</th>
                             <th>Status</th>
+                            <th>Approve</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             payments.map((payment, idx) => <tr key={idx} className="hover">
                                 <th>{idx + 1}</th>
+                                <td>{payment.email}</td>
                                 <td>${payment.price}</td>
                                 <td>{payment.transactionId}</td>
+                                <td>{payment.date}</td>
                                 <td>{payment.status}</td>
+                                <td><button
+                                    onClick={() => handleAprovePayment(payment.email, payment._id)}
+                                    className='btn'
+                                    disabled={payment.status === "approved"}
+                                >Approve</button></td>
                             </tr>)
                         }
 
--- src/components/Pages/Pricing/CheckOut.jsx
+++ src/components/Pages/Pricing/CheckOut.jsx
@@ -118,6 +118,7 @@ const CheckOut = () => {
                     <button
                         className='btn btn-primary'
                         type='submit'
+                        disabled={!stripe || !clientSecret}
                     >
                         Pay
                     </button>
